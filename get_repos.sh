90ef6a9d3c1699052f4049e89c2a103743401665
Tim Kolber <tim.kolber@stud.uni-heidelberg.de>
2024-08-25 13:21:28 +0200

    Fix get_repos.sh

--- get_repos.sh
+++ get_repos.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 echo "Getting Patchscopes from https://github.com/PAIR-code/interpretability/tree/master/patchscopes/code..."
-git clone --no-checkout https://github.com/PAIR-code/interpretability.git
+git clone --no-checkout https://github.com/timkolber/interpretability.git
 cd interpretability
 git sparse-checkout init
 git sparse-checkout set patchscopes/code
 git checkout master
+cd ..
 echo "Getting GLM from https://github.com/Heidelberg-NLP/GraphLanguageModels/tree/main..."
-git clone https://github.com/Heidelberg-NLP/GraphLanguageModels.git
+git clone https://github.com/timkolber/GraphLanguageModels.git
 cd GraphLanguageModels
 wget https://www.cl.uni-heidelberg.de/~plenz/GLM/relation_subgraphs_random.tar.gz
 tar -xvzf relation_subgraphs_random.tar.gz
